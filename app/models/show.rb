157d92ab7bd2ed81fde13684df43208ef05e892e
Karen V <karenvo@usc.edu>
2019-09-20 02:12:15 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,4 @@
 class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :networks
-  def actors_list
-    # returns a list of the full names of each actor associated with the show
-  end
 end
