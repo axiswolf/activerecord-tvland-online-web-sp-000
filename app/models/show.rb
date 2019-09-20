56e80eb3df251b1157ceb6fd1b8c93298e9b747e
Karen V <karenvo@usc.edu>
2019-09-20 02:18:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :networks
+
+  def actors_list
+    self.characters.collect do |character|
+      "#{actor.first_name} - #{actor.last_name}"
+    end.join
+  end
 end
